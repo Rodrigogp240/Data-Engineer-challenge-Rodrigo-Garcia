b921ac2af62ae864a819492dc9a26616e130a329
Rodrigogp240 <rodrigogp240@gmail.com>
2023-10-03 18:04:50 -0300

    fix Q1

--- modules/helper_Q1.py
+++ modules/helper_Q1.py
@@ -3,34 +3,21 @@ from datetime import datetime
 from collections import Counter
 from typing import List, Tuple
 
-## reading json file, sorting key information, populating date_counter
-def get_data(file_path: str) -> (List[Tuple[datetime.date, str]],Counter):
-    data = []
-    date_counter = Counter()
-    with open(file_path,encoding='utf-8') as f:
+
+def get_top_10_users_for_top_10_dates_time(file_path: str) -> List[Tuple[datetime.date, str]]:
+    user_counter = Counter()
+    with open(file_path, encoding='utf-8') as f:
         for line in f:
             tweet = json.loads(line)
             date = datetime.strptime(tweet['date'], '%Y-%m-%dT%H:%M:%S%z').date()
             user = tweet['user']['username']
-            data.append((date, user))
-            date_counter[date] += 1
-    return data,date_counter
+            user_counter[(date, user)] += 1
 
-def get_top_10_users_for_top_10_dates_time(file_path:str) -> List[Tuple[datetime.date, str]]:
-    data,date_counter = get_data(file_path)
-    top_10_dates = [date for date, _ in date_counter.most_common(10)]
-    
-    top_10_users = [(date, user) for date, user in data if date in top_10_dates]
-    
-    user_counter = Counter(top_10_users)
-    temp_top_10_users = user_counter.most_common(10)
-    top_10_users = [(date, user) for (date, user), _ in temp_top_10_users]
-    
-    return top_10_users
+    top_10_items = user_counter.most_common(10)
+    return [item[0] for item in top_10_items]
 
 def get_top_10_users_for_top_10_dates_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
     user_counter = Counter()
-
     with open(file_path, 'r') as file:
         for line in file:
             record = json.loads(line)
@@ -42,4 +29,5 @@ def get_top_10_users_for_top_10_dates_memory(file_path: str) -> List[Tuple[datet
 
 if __name__ == '__main__':
     file_path = '/home/rodrigo/Proyectos_python/Data-Engineer-challenge-Rodrigo-Garcia/tweets/farmers-protest-tweets-2021-2-4.json'
+    get_top_10_users_for_top_10_dates_memory(file_path)
     get_top_10_users_for_top_10_dates_time(file_path)
