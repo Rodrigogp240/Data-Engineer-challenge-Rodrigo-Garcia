7984da9b96d5d7ef21db688011aa8d308c9a2e4f
Rodrigogp240 <rodrigogp240@gmail.com>
2023-10-03 16:06:26 -0300

    add updated Q2

--- modules/helper_Q2.py
+++ modules/helper_Q2.py
@@ -1,47 +1,42 @@
-import json
-import re
-from collections import Counter
 import multiprocessing
+from collections import Counter
+import emoji
+import json
 
-def get_data(file_path: str):
-    data = []
-    with open(file_path, encoding='utf-8') as f:
-        for line in f:
-            tweet = json.loads(line)
-            data.append(tweet['content'])
-    return data
 
-def find_emojis(tweet_text):
-    emoji_pattern = re.compile(r"[\U0001F600-\U0001F64F"
-                            r"\U0001F300-\U0001F5FF"
-                            r"\U0001F680-\U0001F6FF"
-                            r"\U0001F700-\U0001F77F"
-                            r"\U0001F780-\U0001F7FF"
-                            r"\U0001F800-\U0001F8FF"
-                            r"\U0001F900-\U0001F9FF"
-                            r"\U0001FA00-\U0001FA6F"
-                            r"\U0001FA70-\U0001FAFF"
-                            r"\U0001F004-\U0001F0CF"
-                            r"\U0001F170-\U0001F251]+", flags=re.UNICODE)
-    
-    emojis = emoji_pattern.findall(tweet_text)
-    return emojis
-
-def count_top_emojis(json_file_path, num_processes=4, top_n=10):
-    # Open and read the JSON file
-    tweet_texts = get_data(json_file_path)
-    # Count emoji occurrences using multiprocessing
+# Function to process a chunk of tweet texts and count emojis
+def count_emojis_chunk(chunk):
     emoji_counter = Counter()
+    for tweet_text in chunk:
+        emoji_counter.update(emoji.distinct_emoji_list(tweet_text))
+    return emoji_counter
+
+def count_top_emojis_time(json_file_path, num_processes=4):
+    tweet_texts = []
+    with open(json_file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            tweet_data = json.loads(line)
+            tweet_texts.append(tweet_data['content'])
+
+
+    # Split the data into chunks for multiprocessing
+    chunk_size = len(tweet_texts) // num_processes
+    chunks = [tweet_texts[i:i + chunk_size] for i in range(0, len(tweet_texts), chunk_size)]
+
+    # Count emoji occurrences using multiprocessing
     with multiprocessing.Pool(processes=num_processes) as pool:
-        emoji_lists = pool.map(find_emojis, tweet_texts)
-        for emojis in emoji_lists:
-            emoji_counter.update(emojis)
+        emoji_counters = pool.map(count_emojis_chunk, chunks)
 
-    # Get the top N most common emojis
-    top_n_emojis = emoji_counter.most_common(top_n)
+    # Combine results from different processes
+    combined_counter = Counter()
+    for emoji_counter in emoji_counters:
+        combined_counter.update(emoji_counter)
 
-    return top_n_emojis
+    return combined_counter.most_common(10)
+
+
 
 if __name__ == '__main__':
     file_path = '/home/rodrigo/Proyectos_python/Data-Engineer-challenge-Rodrigo-Garcia/tweets/farmers-protest-tweets-2021-2-4.json'
-    top_10_emojis = count_top_emojis(file_path)
+    top_10_emojis2 = count_top_emojis_time(file_path)
+    print(top_10_emojis2)
